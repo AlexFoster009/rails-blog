f678c9b18deb83ca477b0f8a8da2ab7c8aae126c
Alex Foster <afoster009@hotmail.ca>
2015-11-17 02:08:37 +0000

    added more CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,15 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -16,11 +24,23 @@ class ArticlesController < ApplicationController
     end
 
   end
-  
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      flash[:notice] = "You have Successfully updated the article"
+      redirect_to article_path(@article)
+
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   private
     def article_params
     params.require(:article).permit(:title, :description)
