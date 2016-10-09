b442e970ccda99d3778b4a45298acd3aeebb4fff
afoster009 <alex@calgaryscale.com>
2016-10-09 17:47:03 +0000

    Sign in upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @article.user = current_user
+
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "welcome to my Rails Blog! #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render "new"
     end
